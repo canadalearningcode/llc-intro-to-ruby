deb3bf2d4a7d13ea027646703612ad76c14fc174
Jakob Leben <jakob.leben@gmail.com>
2019-04-09 21:50:34 -0700

    Final exercise: Clarify instructions and simplify solution

---
+++ exercises/final_exercise.rb
@@ -0,0 +1,25 @@
+require 'csv'
+
+# Function 'average_rent':
+#     Find average rent for a specific type of rental units
+#     in a specific province and in a specific decade.
+# Arguments:
+#     - data: CSV data returned by CSV.read
+#     - selected_type: Type of rental unit of interest (for example "One bedroom units")
+#     - selected_province: Province of interest (for example "Alberta")
+#     - decade: First year in the decade of interest (for example 1980)
+
+def average_rent(data, selected_type, selected_province, decade)
+    # Complete
+end
+
+# Read data from the CSV file 'rent-data.csv'
+
+# Pick a province and a rental unit type (take a look at the CSV file for examples)
+
+# Call the function 'average_rent' to get the average rent for the desired province and rental unit type in the 80s.
+# Print this value.
+
+# Do the same for the 90s.
+
+# Calculate and print the difference between the average rent in the 90s and in the 80s.
