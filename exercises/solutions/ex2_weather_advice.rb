c2ac24b984bad6d225cc3a8dc728bcc0bd715446
Robin Miller <robin@tenjin.ca>
2019-04-28 22:45:05 -0600

    Removed unnecessary brackets around if statements

--- exercises/solutions/ex2_weather_advice.rb
+++ exercises/solutions/ex2_weather_advice.rb
@@ -2,9 +2,9 @@ puts "What is the weather? (cold, raining, etc.): "
 # chomp removed the enter at the end of a line "chomps" off the /n or ENTER key input
 weather = gets.chomp
 
-if (weather == "cold")
+if weather == "cold"
     puts "Wear a sweater!"
-elsif (weather == "raining")
+elsif weather == "raining"
     puts "Bring an umbrella"
 else
     puts "Dress normally :)"
--- exercises/solutions/ex5_city_rent_comparison.rb
+++ exercises/solutions/ex5_city_rent_comparison.rb
@@ -6,18 +6,21 @@ data = CSV.read('rent-data.csv')
 
 # Variables tracking the city with the highest rent seen so far in the loop
 highest_rent = 0
+
 highest_rent_city_name = ""
 
 # Loop through each of the rows
 for row in data
     # If this row is for a city of interest
-    if ((row.include?("Edmonton, Alberta") || row.include?("Toronto, Ontario")) && row.include?("2008"))
+    if (row.include?("Edmonton, Alberta") || row.include?("Toronto, Ontario")) && row.include?("2008")
         # Print the row
         puts row.to_s + "\n"
+
         # If the rent is the highest so far
         if row[7].to_i > highest_rent
             # Save the rent and the city name in variables
             highest_rent_city_name = row[1].to_s
+
             highest_rent = row[7].to_i
         end
     end
