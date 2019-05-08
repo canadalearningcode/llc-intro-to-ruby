688a3e95f2b608a5b2334ce36cd5af69ba96586d
Robin Miller <robin@tenjin.ca>
2019-05-07 21:02:26 -0600

    Fixed exercise 3 solution

--- exercises/solutions/ex3_smarter_weather.rb
+++ exercises/solutions/ex3_smarter_weather.rb
@@ -1,12 +1,13 @@
-puts "What is the weather? (cold, raining, etc.): "
-# chomp removed the enter at the end of a line "chomps" off the /n or ENTER key input
-weather = gets.chomp.to_i
+puts "What is the temperature? (enter a number): "
 
-# if the weather is greater than or equal to 25 degrees
-if weather >= 25
+# chomp removed the enter at the end of a line "chomps" off the \n or ENTER key input
+temperature = gets.chomp.to_i
+
+# if the temperature is greater than or equal to 25 degrees
+if temperature >= 25
     puts "Go to the beach!"
-# the weather is less than 25 degrees AND greater than 15 degrees
-elsif weather < 25 && weather > 15
+# the temperature is less than 25 degrees AND greater than 15 degrees
+elsif temperature < 25 && temperature > 15
     # Still warm enough for ice cream!
     puts "I'm getting ice cream!"
 else
